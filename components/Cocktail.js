ed915fdb87855027ac10f8102107418b4271a9ae
Danilo Moura <danilo.moura@luizalabs.com>
2023-09-22 17:28:25 -0300

    efeito hover cocktail card

--- components/Cocktail.js
+++ components/Cocktail.js
@@ -2,14 +2,16 @@ import React from "react";
 
 function Cocktail({ name, image, description }) {
   return (
-    <div className="bg-white p-4 my-4 rounded-lg shadow-md w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4">
-      <h2 className="text-gray-950 text-center">{name}</h2>
-      <img
-        className="block mx-auto max-w-full h-auto rounded-lg"
-        src={image}
-        alt={name}
-      />
-      <p className="text-black line-clamp-10 truncate">{description}</p>
+    <div className="bg-white p-4 my-4 rounded-lg shadow-md w-1/2 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 transition-transform hover:scale-105 hover:bg-slate-400 cursor-pointer text-white">
+      <a href="#" className="no-underline">
+        <h2 className="text-gray-950 text-center">{name}</h2>
+        <img
+          className="block mx-auto max-w-full h-auto rounded-lg"
+          src={image}
+          alt={name}
+        />
+        <p className="text-black line-clamp-10 truncate">{description}</p>
+      </a>
     </div>
   );
 }
