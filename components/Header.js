6ff4c58114f9720f45d4e0e3ef9255c966854ca9
Danilo Moura <danilo.moura@luizalabs.com>
2023-09-25 15:15:18 -0300

    logo ahref home

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,13 @@ import React from "react";
 
 function Header() {
   return (
-<header className="bg-gray-800 text-white py-2">
-  <div className="flex items-center justify-center">
-    <img src="/logo.png" alt="Logo" className="w-15 h-15 mr-2" />
-    
-  </div>
-</header>
+    <header className="bg-gray-800 text-white py-2">
+      <div className="flex items-center justify-center">
+        <a href="/">
+          <img src="/logo.png" alt="Logo" className="w-15 h-15 mr-2" />
+        </a>
+      </div>
+    </header>
   );
 }
 
